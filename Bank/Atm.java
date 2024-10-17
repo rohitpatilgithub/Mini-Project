15bfe150bc6825811fee589d5a8bbe366a54ae8e
Rohit <flux.amv25@gmail.com>
2024-10-17 14:13:15 +0530

    Modified Atm.java

--- Bank/Atm.java
+++ Bank/Atm.java
@@ -4,38 +4,40 @@ public class Atm extends User{
 
     int input;
     int acc;
-    Current one = new Current();
+    private Current one = new Current();
 
     void getAcc(){
         System.out.println("Enter\n1. Savings Account \n2. Current Account");
         acc = sc.nextInt();
         if(acc == 1){
-            while(input != 6){
+            while(input != 7){
                 System.out.println("Enter \n1. Create Profile\n2. Update Profile\n" +
-                        "3. Withdraw money\n4. Apply loan\n5. Current loan\n6. Exit");
+                        "3. Show profile\n4. Withdraw money\n5. Apply loan\n6. Current loan\n7. Exit");
                 input = sc.nextInt();
                 switch (input){
                     case 1 -> this.createProfile();
                     case 2 -> this.updateProfile();
-                    case 3 -> this.withdraw();
-                    case 4 -> this.applyLoan();
-                    case 5 -> this.currentLoan();
-                    case 6 -> System.out.println("Exit");
+                    case 3 -> this.showProfile();
+                    case 4 -> this.withdraw();
+                    case 5 -> this.applyLoan();
+                    case 6 -> this.currentLoan();
+                    case 7 -> System.out.println("Exit");
                     default -> System.out.println("Error : Enter valid command");
                 }
             }
         } else if (acc == 2) {
-            while(input != 6){
+            while(input != 7){
                 System.out.println("Enter \n1. Create Profile\n2. Update Profile\n" +
-                        "3. Withdraw money\n4. Apply loan\n5. Current loan\n6. Exit");
+                        "3. Show profile\n4. Withdraw money\n5. Apply loan\n6. Current loan\n7. Exit");
                 input = sc.nextInt();
                 switch (input){
                     case 1 -> one.createProfile();
                     case 2 -> one.updateProfile();
-                    case 3 -> one.withdraw();
-                    case 4 -> one.applyLoan();
-                    case 5 -> one.currentLoan();
-                    case 6 -> System.out.println("Exit");
+                    case 3 -> one.showProfile();
+                    case 4 -> one.withdraw();
+                    case 5 -> one.applyLoan();
+                    case 6 -> one.currentLoan();
+                    case 7 -> System.out.println("Exit");
                     default -> System.out.println("Error : Enter valid command");
                 }
             }
